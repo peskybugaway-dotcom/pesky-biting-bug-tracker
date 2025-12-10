d18c51c94a2227da6f657397497596abeb79e5ba
peskybugaway-dotcom <peskybugaway@gmail.com>
2025-12-10 17:59:36 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
+window.addEventListener("error", (e) => {
+  document.body.innerHTML =
+    "<pre style='color:red;background:#111;padding:20px;white-space:pre-wrap;'>" +
+    e.message +
+    "</pre>";
+});
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
