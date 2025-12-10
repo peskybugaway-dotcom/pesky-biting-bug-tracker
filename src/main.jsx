b95f30f05dd2ca865f948aec33a826fdcc593286
peskybugaway-dotcom <peskybugaway@gmail.com>
2025-12-10 18:06:29 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+// Global crash catcher (even outside React)
+window.addEventListener("error", (e) => {
+  document.body.innerHTML =
+    "<pre style='color:red;background:#111;padding:20px;white-space:pre-wrap;'>" +
+    "Global Error: " + e.message +
+    "</pre>";
+});
+
+const root = document.getElementById("root");
+createRoot(root).render(<App />);
