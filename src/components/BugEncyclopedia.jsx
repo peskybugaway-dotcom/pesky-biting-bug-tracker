b57d90637c676b67ac40ff4fbb8b6b8e7bc25450
peskybugaway-dotcom <peskybugaway@gmail.com>
2025-12-15 12:32:23 -0500

    Update BugEncyclopedia.jsx

--- src/components/BugEncyclopedia.jsx
+++ src/components/BugEncyclopedia.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import {
-  Bug,
-  Mosquito,
-  Skull,
-  AlertTriangle,
-  ShieldAlert
-} from "lucide-react";
+import { Bug, Mosquito, Skull, AlertTriangle } from "lucide-react";
 
 const BUGS = [
-  // -------------------------------------------------
-  // MOSQUITOES
-  // -------------------------------------------------
   {
     name: "Aedes Aegypti (Yellow Fever Mosquito)",
     type: "Mosquito",
@@ -18,10 +9,4 @@ const BUGS = [
     icon: <Mosquito className="w-6 h-6 text-red-400" />,
     image: "https://i.imgur.com/J1v7C3d.jpeg",
     description:
-      "Aggressive day-biting mosquito that spreads Dengue, Zika, and Yellow Fever. Thrives in urban Florida environments and small water containers."
-  },
-  {
-    name: "Aedes Albopictus (Asian Tiger Mosquito)",
-    type: "Mosquito",
-    danger: "High",
-    icon: <Mos
+      "Agg
